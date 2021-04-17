804f92783203d1e3d4f537154c67ff89870cf8a5
Rodrigo Gouveia <rsoares.developer@gmail.com>
2021-04-17 11:38:29 -0400

    :sparkles: Hello, World

---
+++ hello-world/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+import "rsc.io/quote"
+
+//Tutorial: Get started with Go - https://golang.org/doc/tutorial/getting-started#code
+func main() {
+	fmt.Println(quote.Go())
+}
